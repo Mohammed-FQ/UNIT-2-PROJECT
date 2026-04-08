1cedf288a27dea335fcefe8eb2987b04242ad946
Mohammed Alqadda <MohammedFAlqadda@gmail.com>
2026-04-08 23:08:00 +0300

    finished base and setup other pages

--- GreenerSaudi/main/urls.py
+++ GreenerSaudi/main/urls.py
@@ -5,6 +5,13 @@ from . import views
 app_name ='main'
 urlpatterns = [
     path('', views.home_view, name='home_view'),
+    path('about/', views.about_view, name='about_view'),
+    path('impact/', views.impact_view, name='impact_view'),
+    path('progress/', views.progress_view, name='progress_view'),
+    path('future/', views.future_view, name='future_view'),
+    path('get-involved/', views.get_involved_view, name='get_involved_view'),
+    path('updates/', views.updates_view, name='updates_view'),
+    path('contact/', views.contact_view, name='contact_view'),
     path('base/', views.base_view, name='base_view'),
 ]
 
--- GreenerSaudi/main/views.py
+++ GreenerSaudi/main/views.py
@@ -4,5 +4,26 @@ from django.http import HttpResponse, HttpRequest
 def home_view(request: HttpRequest):
     return render(request, 'main/home.html')
 
+def about_view(request: HttpRequest):
+    return render(request, 'main/about.html')
+
+def impact_view(request: HttpRequest):
+    return render(request, 'main/impact.html')
+
+def progress_view(request: HttpRequest):
+    return render(request, 'main/progress.html')
+
+def future_view(request: HttpRequest):
+    return render(request, 'main/future.html')
+
+def get_involved_view(request: HttpRequest):
+    return render(request, 'main/get_involved.html')
+
+def updates_view(request: HttpRequest):
+    return render(request, 'main/updates.html')
+
+def contact_view(request: HttpRequest):
+    return render(request, 'main/contact.html')
+
 def base_view(request: HttpRequest):
     return render(request, 'main/base.html')
